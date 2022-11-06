fa6c042e0dea7f668cc45801c68346bbaadfd9ef
Renato Medeiros <renamedrj@gmail.com>
2022-11-06 17:46:22 +0000

    Feature/add db support

---
+++ Bracabot2/Domain/Responses/TwitchApiStreamInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bracabot2.Domain.Responses
+{
+    public class TwitchApiStreamInfoResponse
+    {
+        [JsonPropertyName("data")]
+        public List<TwitchApiStreamInfoNodeResponse> Data { get; set; }
+    }
+}
